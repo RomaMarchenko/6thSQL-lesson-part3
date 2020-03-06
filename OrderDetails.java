a304fa020e1fca02e8e606f32b432dfb820ef56c
RomaMarchenko <52100927+RomaMarchenko@users.noreply.github.com>
2020-03-06 04:04:17 +0200

    Update OrderDetails.java

--- OrderDetails.java
+++ OrderDetails.java
@@ -1,9 +1,17 @@
 package lesson6_hw.part3;
 
 public class OrderDetails {
-    private long orderId;
+    private Orders order;
     private Products product;
     private long unitPrice;
     private long quantity;
     private long discount;
+
+    public OrderDetails(Orders order, Products product, long unitPrice, long quantity, long discount) {
+        this.order = order;
+        this.product = product;
+        this.unitPrice = unitPrice;
+        this.quantity = quantity;
+        this.discount = discount;
+    }
 }
